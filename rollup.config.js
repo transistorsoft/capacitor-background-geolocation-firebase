34176297e81edbdd5d4cb128dd41c98756b9af53
Chris Scott <christocracy@gmail.com>
2021-09-24 10:31:09 -0400

    Fix android plugin packagename in AndroidManifest.  Was referencing bgfetch namespace.  This plugin template was originally copied from background-fetch

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ export default {
     {
       file: 'dist/plugin.js',
       format: 'iife',
-      name: 'capacitorBackgroundFetch',
+      name: 'capacitorBackgroundGeolocationFirebase',
       globals: {
         '@capacitor/core': 'capacitorExports',
       },
